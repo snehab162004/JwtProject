5bba88dfbeabe855c14bbf90351b58ae92a6c672
snehab162004 <72303246+snehab162004@users.noreply.github.com>
2022-07-21 22:35:04 +0530

    Error in Authorization

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const posts = [
      }
 ]
 
-app.get('/posts', (req, res) => {
-     res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req, res) => {
@@ -29,4 +29,16 @@ app.post('/login', (req, res) => {
   res.json({ accessToken : accessToken})
 })
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if(err) return res.sendStatus(403)
+     req.user = user
+     next()
+  })
+  
+}
 app.listen(3000);
